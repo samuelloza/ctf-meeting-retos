77614397bccae2c04c0ef2c2d4bc34efb8e1f369
Samuel Loza <starsaminf@gmail.com>
2023-10-06 17:34:59 -0400

    Update reto4

--- 4headers/index.php
+++ 4headers/index.php
@@ -2,10 +2,10 @@
 $referer = $_SERVER['Referer'];
 
 if (strpos($referer, 'fbi.gov') !== false) {
-    echo 'Tu no eres fbi.gov';
     echo file_get_contents('./sc.html');
 } else {
     echo 'No tienes acceso a esta página.';
+    echo 'Tu no eres fbi.gov';
 }
 
 
